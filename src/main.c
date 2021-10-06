5247f7443beaf20cefc8253c66df6dae9e4957b0
swang <swang@MacBook-sh.local>
2021-10-06 22:44:52 +0900

    update parameter error message

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 19:02:01 by swang             #+#    #+#             */
-/*   Updated: 2021/10/06 16:45:02 by swang            ###   ########.fr       */
+/*   Updated: 2021/10/06 22:29:36 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char *argv[], char **envp)
 	ret = 0;
 	if (argc != 5)
 	{
-		ft_putstr_fd("usage : ./pipex file1 'cmd1' 'cmd2' file2\n", 2);
+		ft_putstr_fd("Error\ncheck parameter\n./pipex file1 \"cmd1\" \"cmd2\" file2\n", 2);
 		return (0);
 	}
 	ret = pipex(argv, envp);
